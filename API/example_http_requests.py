645405f15840e77af3f55ffc60be639ed33445d2
stristanS <svetlanatristan@gmail.com>
2021-12-05 15:45:42 +0300

    readme upd

--- API/example_http_requests.py
+++ API/example_http_requests.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import json
 import numpy as np
+import time
 
 """__________________ ТЕСТ НА ДАННЫХ BOSTON DATASET (РЕГРЕССИЯ)_______________________"""
 
@@ -14,22 +15,21 @@ print(response, response.json())
 """Вывод моделей, доступных для обучения"""
 response_1 = requests.get('http://127.0.0.1:8080/post_data')
 print(response_1, response_1.json())
-#
+
 """Обучение модели 1"""
 model_params = {'fit_intercept': True, 'normalize': True}
 response_2 = requests.post('http://127.0.0.1:8080/train_model/1', json = model_params)
 print(response_2, response_2.json())
-#
+
 """Предсказание модели 1"""
 payload = (pd.read_csv('data/boston_test.csv', index_col = 0).to_json())
 response_3 = requests.post('http://127.0.0.1:8080/predict/1', json =payload)
 print(response_3, response_3.json())
-#
 
 """CREATE collection для метрики модели 1"""
 response_3_2 = requests.get('http://127.0.0.1:8080/create_metric/1')
 print(response_3_2, response_3_2.json())
-# #
+
 """UPDATE метрики модели 1"""
 df = pd.read_csv('data/boston_test.csv', index_col = 0)
 target_col_name = 'medv'
@@ -38,10 +38,11 @@ data = {'payload': payload}
 response_3_1 = requests.post('http://127.0.0.1:8080/metric/1', json= data)
 print(response_3_1, response_3_1.json())
 
+time.sleep(1)
 """Статус воркера для расчета метрики модели 1"""
-response_3_2 = requests.post('http://127.0.0.1:8080/results/3ea4cb7a-15a3-4176-8ea7-0b70272f416c')
-print(response_3_2, response_3_2.json())
-#
+response_3_2_2 = requests.post('http://127.0.0.1:8080/results/'+str(response_3_1.json()[10:]))
+print(response_3_2_2, response_3_2_2.json())
+
 """GET метрики модели 1"""
 response_3_2 = requests.get('http://127.0.0.1:8080/get_metric/1')
 print(response_3_2, response_3_2.json())
@@ -94,9 +95,10 @@ data = {'payload': payload}
 response_3_1 = requests.post('http://127.0.0.1:8080/metric/2', json= data)
 print(response_3_1, response_3_1.json())
 
+time.sleep(1)
 """Статус воркера для расчета метрики модели 2"""
-response_3_2 = requests.post('http://127.0.0.1:8080/results/733e0a14-aa32-49c9-b1cb-28cd71c52c1f')
-print(response_3_2, response_3_2.json())
+response_3_2_2 = requests.post('http://127.0.0.1:8080/results/'+str(response_3_1.json()[10:]))
+print(response_3_2_2, response_3_2_2.json())
 
 """GET метрики модели 2"""
 response_3_2 = requests.get('http://127.0.0.1:8080/get_metric/2')
